898173274e6ccc8aba6fce548b1b783e277b5619
DACHIMG <Dachimgaloblishvili@gmail.com>
2025-11-08 03:45:36 -0800

    second commit

--- DAY001/classwork/cw2.py
+++
@@ -1,3 +0,0 @@
-num1=7
-num2=6
-print(num1+num2 )
---
+++ DAY004/homework/hw.py
@@ -0,0 +1,13 @@
+# case_sensitive არის მაგალითად დავწერეთ num(1) Num(14) print(num) ჩვენ Num(14) კი არ გამოგვიტანს ჩვენ გამოგვიტანს_num(1)_ს
+# snake_case არის მაგალითად brother_hood snake_case სულ _ თი იწერება
+# იმიტომ, რომ კომენტარით რაღაცის გაკეთება არ შეგიძლია უბრალოდ დასაწერად არის კარგი
+name="my_brother"
+print(name)
+name="brother_hood"
+print(name)
+name="my_sister"
+print(name)
+name="my_mother"
+print(name)
+name="my_dad"
+print(name)
