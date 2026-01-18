77a177a24bf60c717795c9d152785076c3d7e0c0
DACHIMG <Dachimgaloblishvili@gmail.com>
2026-01-17 22:19:26 -0800

    second commit

--- DAY10/homework/hw.py
+++ DAY10/homework/hw.py
@@ -1,7 +1,19 @@
 # input არის რაღაცის შეტანა
 # output არის რაღაცის გამოტანა
-# type() ფუნქციას შეუძლია, რომ სტრინგი გადააქციოს ინტეგერად
+# type() ამოწმებს ფრჩხილებში ჩაწერილ რაღაცას
 # int() strings გადააქცევს integer_ად
 # str() floats გადააქცევს string_ად
 # float() integers გადააქცევს float_ად
 
+name = input(" Enter your name: ")
+print("hello " + name)
+print(name)
+
+num = int(input(" Enter any number: "))
+word = input(" Enter any word: ")
+print(num * word)
+
+name = input(" Enter your name: ")
+surname = input(" Enter your surname: ")
+age = input(" Enter your age: ")
+print("your name " + " your surname" + "your age")
---
+++ DAY14/homework/hw.py
@@ -0,0 +1,20 @@
+# ბულეანი შედგება 2 რაღაცისგან - truth, და false_ი
+# შედარების ოპერაცია გვეხმარება რომ გავიგოთ რომელი ამოცანა არის მართალი და რომელი არის მცდარი
+
+print(10 > 5)
+print(20 < 35)
+print(39 > 29)
+print(40 < 57)
+print(70 > 69)
+
+print(110 < 90)
+print(10 > 100)
+print(60 < 7)
+print(100 > 110000)
+print(20 < 9)
+
+age = int(input(" Enter your age: "))
+print(age > 18)
+
+age = int(input(" Enter your age: "))
+print(age < 18)
