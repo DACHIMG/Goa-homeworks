9b2f65631749966f27f004fbcd6170753610866b
DACHIMG <Dachimgaloblishvili@gmail.com>
2026-02-22 03:10:26 -0800

    second commit

---
+++ DAY20/classwork/cw.py
@@ -0,0 +1,12 @@
+# while loop_ი იმეორებს სანამ შეშმარიტი არ იქნება. for loop_ი იმეორებს რამდენჯერაც გინდა
+
+password = "dachi2016"
+user_guess = ""
+
+while password != user_guess: 
+ user_guess = input(" guess the password: ")
+ print("guess until you get it right")
+print("you guess it")
+
+age = input(" what is your age: ")
+
