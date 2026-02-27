c4740b377bc5c2aa09c38070c0f5280c28af6917
DACHIMG <Dachimgaloblishvili@gmail.com>
2026-02-27 05:54:04 -0800

    second commit

---
+++ DAY20/homework/cw.py
@@ -0,0 +1,18 @@
+# if else გვჭირდებაა, რომ გვითხრას რომელი ფორმაა სწორი
+
+num = 10
+if num > 0:
+ print("num aris meti 0 ze")
+else:
+  print("num ar aris meti 0 ze")
+
+password = input(" Enter your password: ")
+if password == "dachi2016":
+  print("password is corect!")
+else:
+  print("incorect password!")
+
+number = input(" Enter any number: ")
+print(number)
+
+print(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)
