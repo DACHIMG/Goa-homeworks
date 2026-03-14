202f6191d0c413d06f640d6015eebeddb9708d68
DACHIMG <Dachimgaloblishvili@gmail.com>
2026-03-14 14:52:34 +0400

    second commit

---
+++ DAY22/classwork/cw.py
@@ -0,0 +1,12 @@
+
+name = ("dachi")
+for i in name:
+   print(i)
+
+count = (0, 1, 2, 3, 4, 5)
+print(0 + 1 + 2 + 3 + 4 + 5)
+print("დაფიქსირდა 5_ჯერ")
+
+
+
+
---
+++ DAY22/homework/cw.py
@@ -0,0 +1,34 @@
+# if - ვიყენებთ პირველი პირობის შესამოწმებლად
+# elif - ვიყენებთ დამატებითი პირობების შესამოწმებლად
+# else - თუ არცერთი პირობა არ შესრულდება 
+
+age = input(" what is your age: ")
+if age > 18:
+    print("university")
+elif age == 7:
+    print("school")
+else:
+    print("kindergarten")
+
+score = input(" what is your score: ")
+if score == 100:
+    print("A group")
+elif score == 89-99:
+    print("B Group")
+elif score == 40-70:
+    print("C group")
+else:
+    print("D Group")
+
+name = input(" what is your name: ")
+if name == vocals:
+  print("here vocals")
+else:
+    print("no vocals")
+
+age = input(" what is your age in 20 years ")
+if age == 40:
+  print("you got older")
+else:
+    print("you will get older")
+ 
