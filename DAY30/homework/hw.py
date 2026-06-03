ddd0c41779135f3d32dbebded99bf3931142dbde
DACHIMG <Dachimgaloblishvili@gmail.com>
2026-06-03 15:19:31 +0400

    second commit

---
+++ DAY30/homework/hw.py
@@ -0,0 +1,19 @@
+colors = ["red", "green", "blue", "yellow", "purple"]
+print(colors[0])
+print(colors[-1])
+print(colors[2])
+
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+print(numbers[2:7])
+print(numbers[:4])
+print(numbers[-3:])
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(letters[::2])
+print(letters[2:7:2])
+
+word = ["p", "y", "t", "h", "o", "n"]
+print(word[::-1])
+
+animals = {"dog", "cat", "mouse", "bear", "wolf"}
+animals[1:4] = ["lion", "tiger"]
